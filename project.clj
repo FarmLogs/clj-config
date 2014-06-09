666ffd505e7673877f116d5cbbad51959694c246
Stuart Hinson <stuart.hinson@gmail.com>
2014-06-09 11:53:30 -0400

    [sh][72782760] include primedia repository

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject clj-config "0.1.0-SNAPSHOT"
+(defproject clj-config "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :repositories [["primedia"
+                  {:url "http://nexus.idg.primedia.com/nexus/content/repositories/primedia"
+                   :sign-releases false}]])
