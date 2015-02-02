e211dd023afeb2fa86557f0dbbfda14854d660ec
Stuart Hinson <stuart.hinson@gmail.com>
2015-02-02 15:36:02 -0500

    [sh][87456002] correct display of missing app config
    prev approach did not work since app-config is a nested map and
    required-app-config a sequence of paths for config values

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-config "0.4.1"
+(defproject clj-config "0.4.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
