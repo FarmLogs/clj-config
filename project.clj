79cf5f749e1906730c5b4cc8a1da286d0e4a3a2f
Brian Rowe <browe@rentpath.com>
2015-05-18 17:28:08 -0400

    [BR][94859930] Bump version to 0.4.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-config "0.4.3"
+(defproject clj-config "0.4.4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
