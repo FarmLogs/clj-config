a5198c48b81d04fc83c31338e3358de764cd0239
Brian Rowe <brianrowe@farmlogs.com>
2016-01-12 16:58:46 -0500

    Bump major version to 1.0.0
    Changed how we represent config. From:
    
    ```clojure
    (defconfig
      :env {foo "FOO_VAR"
            bar "BAR_VAR"}
      :app {baz [:some :key :path]})
    ```
    
    To:
    
    ```clojure
    (defconfig
      :env [[foo "FOO_VAR"]
            [bar "BAR_VAR"]]
      :app [[baz [:some :key :path]]])
    ```

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-config "0.4.4"
+(defproject clj-config "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
